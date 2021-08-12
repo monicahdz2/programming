37feb98fc49850bafe5159fcd737eace08dd2ab7
monicahdz2 <2009069@upy.edu.mx>
2021-08-12 11:54:45 -0500

    ex6-14

---
+++ Unit_3/chapter14ex06.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+
+char *in_file = "formatted-file.txt";
+
+int main() {
+
+	FILE *infile;           /* file pointer to input file */
+	char line[100];         /* a line from the file */
+
+	int position;           /* position into the record */
+
+	infile = fopen(in_file, "r");
+	if (infile == NULL) {
+		fprintf(stderr, "Problem opening %s for reading\n", in_file);
+		return(8);
+	}
+
+	position = 0;
+	while (fgets(line, sizeof(line), infile)) {
+		if (line[0] == '\n') {
+			/* blank line - record separator */
+			printf("\n");
+			position = 0;
+			continue;
+		}
+
+		line[strlen(line) - 1] = '\0';          /* get rid of newline */
+
+		switch (position) {
+			case 1:
+			case 2:
+			case 5:
+			case 6:
+				printf("%s\n", line);       /* last name, street, zip code, phone */
+				break;
+			case 0:
+			case 4:
+				printf("%s ", line);        /* first name, state */
+				break;
+			case 3:
+				printf("%s, ", line);       /* city */
+				break;
+			default:
+				printf("Error: unknown field\n");
+		}
+
+		++position;
+
+	}
+
+	fclose(infile);
+
+	return(0);
+}
